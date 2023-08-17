8f151dbbba641ed57d6002f8803e08309efc3fa2
Caique Sobral <caiquelsobral@gmail.com>
2023-08-17 20:03:31 -0300

    FEAT: create dto pattern

--- src/main/java/com/postsapi/posts/resources/UserResource.java
+++ src/main/java/com/postsapi/posts/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.postsapi.posts.resources;
 
 import com.postsapi.posts.domain.User;
+import com.postsapi.posts.domain.dto.UserDTO;
 import com.postsapi.posts.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -20,9 +22,10 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
 
-        List<User> list = userService.findAll();
-        return ResponseEntity.ok(list);
+        List<UserDTO> dtoList = userService.findAll().
+                stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok(dtoList);
     }
 }
