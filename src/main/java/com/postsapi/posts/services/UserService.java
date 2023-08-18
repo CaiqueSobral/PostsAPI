672648f1a624f8268f1082be073eb97497d65c82
Caique Sobral <caiquelsobral@gmail.com>
2023-08-18 15:35:48 -0300

    FEAT: Insert User

--- src/main/java/com/postsapi/posts/services/UserService.java
+++ src/main/java/com/postsapi/posts/services/UserService.java
@@ -1,6 +1,7 @@
 package com.postsapi.posts.services;
 
 import com.postsapi.posts.domain.User;
+import com.postsapi.posts.domain.dto.UserDTO;
 import com.postsapi.posts.repository.UserRepository;
 import com.postsapi.posts.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,13 @@ public class UserService {
         }
 
         return user.get();
+    }
+
+    public User insert(User obj){
+        return userRepo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
