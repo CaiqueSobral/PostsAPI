917f4ad6a0d5b4abdc8f83b1016d8fc2cd62db8d
Caique Sobral <caiquelsobral@gmail.com>
2023-08-18 18:58:03 -0300

    FEAT: add comments to the posts

--- src/main/java/com/postsapi/posts/config/Instantiation.java
+++ src/main/java/com/postsapi/posts/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.postsapi.posts.config;
 import com.postsapi.posts.domain.Post;
 import com.postsapi.posts.domain.User;
 import com.postsapi.posts.domain.dto.AuthorDTO;
+import com.postsapi.posts.domain.dto.CommentDTO;
 import com.postsapi.posts.repository.PostRepository;
 import com.postsapi.posts.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,6 @@ public class Instantiation implements CommandLineRunner {
     private UserRepository userRepo;
     @Autowired
     private PostRepository postRepo;
-
 
     @Override
     public void run(String... args) throws Exception {
@@ -42,7 +42,15 @@ public class Instantiation implements CommandLineRunner {
         Post post3 = new Post(null, sdf.parse("14/09/2019"), "Emprego novo",
                 "To de emprego novo", new AuthorDTO(bob));
 
+        CommentDTO cmm1 = new CommentDTO("Boa viagem!!!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO cmm2 = new CommentDTO("Aproveitaaa bjs!!!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO cmm3 = new CommentDTO("Baum dia", sdf.parse("25/05/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(cmm1, cmm2));
+        post2.getComments().add(cmm3);
+
         postRepo.saveAll(Arrays.asList(post1, post2, post3));
+
         maria.getPosts().addAll(Arrays.asList(post1, post2));
         bob.getPosts().add(post3);
         userRepo.saveAll(Arrays.asList(maria, bob));
